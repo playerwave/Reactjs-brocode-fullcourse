db8e991dbeec3a513a281c593fab2c4d4aefc5de
Thanaphat Ketsani <65160169@go.buu.ac.th>
2024-04-20 22:12:19 +0700

    Header & Footer

---
+++ src/Header.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+function Header() {
+  return (
+    <div>
+      <h1>My Website</h1>
+      <nav>
+        <ul>
+          <li>Home</li>
+          <li>About</li>
+          <li>Service</li>
+          <li>Contact</li>
+        </ul>
+      </nav>
+      <hr />
+    </div>
+  );
+}
+
+export default Header;
